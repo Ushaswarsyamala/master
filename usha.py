a74596778bcf8c5c725c1ceacdd9bc9006fc577a
Ushaswarsyamala <ushachinnimudhuru01@gmail.com>
2025-04-25 13:31:13 +0530

    deleted

--- usha.py
+++
@@ -1,8 +0,0 @@
-#print("radha krishna")
-#print ("Hare Krishna")
-#Wap to input users first name and print its length of firtname
-#firstname= input("first name="usha )
-#print(len(firstname))
-# wap to find the occurance of $ in a string
-name= "hlo $ hlo $ ,doller=$"
-print(name.count("$"))
